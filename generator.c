df9c190ac7470e4b4f138538f7a46667219528a6
Aaron Bager <kingjojo3@gmail.com>
2021-02-22 17:37:00 -0500

    Setting up parent and child

--- generator.c
+++ generator.c
@@ -16,7 +16,8 @@ int main(int argc, char** argv) {
 	// struct range *ranges;  // struct to hold all the ranges 
 	int** parent_write_fd; // for the parent to write to the children 
        	int** child_write_fd; 	// for the children to write to the parent
-	
+	parent_write_fd = (int**)malloc(argc*sizeof(int*)); 
+	child_write_fd = (int**)malloc(argc*sizeof(int*)); 
 	// lower_range = (int*)malloc(argc*sizeof(int)); 
 	// upper_range = (int*)malloc(argc*sizeof(int));
 	ranges = (struct range*)malloc(argc*sizeof(struct range)); // allocate space for each range 
@@ -24,8 +25,10 @@ int main(int argc, char** argv) {
 	for ( int i = 1; i < argc; ++i) {
 		(ranges+i)->lower = atoi(strtok(argv[i], ":")); // place lower range into struct 
 	       	(ranges+i)->upper = atoi(strtok(NULL, ":"));  // place upper range into struct
-		*(parent_write_fd+i) = (int*)malloc(argc*sizeof(int)); // create pipe for parent to write to child 
-		*(child_write_fd+i) = (int*)malloc(argc*sizeof(int)); // create pipe for child to write to parent 
+		printf("Lower Range: %i\n", (ranges+i)->lower); 
+		printf("Upper Range: %i\n", (ranges+i)->upper); 
+		*(parent_write_fd+i) = (int*)malloc(2*sizeof(int)); // create pipe for parent to write to child 
+		*(child_write_fd+i) = (int*)malloc(2*sizeof(int)); // create pipe for child to write to parent 
 	       	pipe(*(parent_write_fd+i)); // pipe system call for parent to child 
 		pipe(*(child_write_fd+i)); // pipe system call for child to parent 	
 		if ( (ranges+i)->lower >= (ranges+i)->upper) { // if the lower range is larger than the upper range, then the program will throw an error
@@ -55,11 +58,15 @@ int main(int argc, char** argv) {
 	// int children = argc; 
 	
 	for ( int i = 1; i < argc; ++i) {
-		if(*(process_id+i) == 0) {
-			printf("%i\n", *(process_id+i)); 
+		if(*(process_id+i) == 0) { // child 
+			close(*(*(fd+1)+j)); // closing write end of pipe, using different pipe to write to parent  
 		}
-		else {
+		else { // parent 
 			printf("Parent\n"); 
+			for ( int j = 1; j < argc; ++j) {
+				close(*(*(fd)+j)); // close read end of pipe, using different pipe to get information from child
+				write(*(*(fd+1)+j), (ranges+j), sizeof(struct range)); // give each child a struct to work with   
+			}
 		}
 	}	
 	
