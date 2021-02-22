ea93d2b05e8f4bdf92f07b4a04f9c454f96960ba
Aaron Bager <kingjojo3@gmail.com>
2021-02-22 17:16:27 -0500

    Created struct instead of two seperate dynamic arrays

--- generator.c
+++ generator.c
@@ -4,47 +4,73 @@
 #include <unistd.h>
 #include <stdlib.h> 
 
+struct range {
+	int lower;
+	int upper; 
+} *ranges;
+
 int main(int argc, char** argv) {
-	pid_t *process_id; 
-	int* lower_range; 
-	int* upper_range; 
-	int** fd; 
+	pid_t *process_id; // dynamic array to hold all the process ids 
+	// int* lower_range; 
+	// int* upper_range;
+	// struct range *ranges;  // struct to hold all the ranges 
+	int** parent_write_fd; // for the parent to write to the children 
+       	int** child_write_fd; 	// for the children to write to the parent
 	
-	lower_range = (int*)malloc(argc*sizeof(int));
-	upper_range = (int*)malloc(argc*sizeof(int)); 
-	fd = (int**)malloc(argc*sizeof(int*));  
-	for ( int i = 1; i < argc; i++) {
-		*(lower_range+i) = atoi(strtok(argv[i], ":"));
-	       	*(upper_range+i) = atoi(strtok(NULL, ":")); 
-		*(fd+i) = (int*)malloc(argc*sizeof(int)); 
-		pipe(*(fd+i)); 
-		if ( *(lower_range+i) >= *(upper_range+i) ) {
-			free(lower_range);
-			free(upper_range); 
-			free(fd);	
+	// lower_range = (int*)malloc(argc*sizeof(int)); 
+	// upper_range = (int*)malloc(argc*sizeof(int));
+	ranges = (struct range*)malloc(argc*sizeof(struct range)); // allocate space for each range 
+	//fd = (int**)malloc(argc*sizeof(int*));  
+	for ( int i = 1; i < argc; ++i) {
+		(ranges+i)->lower = atoi(strtok(argv[i], ":")); // place lower range into struct 
+	       	(ranges+i)->upper = atoi(strtok(NULL, ":"));  // place upper range into struct
+		*(parent_write_fd+i) = (int*)malloc(argc*sizeof(int)); // create pipe for parent to write to child 
+		*(child_write_fd+i) = (int*)malloc(argc*sizeof(int)); // create pipe for child to write to parent 
+	       	pipe(*(parent_write_fd+i)); // pipe system call for parent to child 
+		pipe(*(child_write_fd+i)); // pipe system call for child to parent 	
+		if ( (ranges+i)->lower >= (ranges+i)->upper) { // if the lower range is larger than the upper range, then the program will throw an error
+			// free(lower_range);
+			// free(upper_range);
+			free(ranges); 
+		       	free(process_id); 	
+			free(parent_write_fd);
+			free(child_write_fd);	
 			perror("LowerRange:UpperRange, Lower range must be smaller than Upper Range!"); 
 			return -1; 
 		}
 
 	}
+		
+
+	process_id = (pid_t*)malloc(argc*sizeof(pid_t)); // allocating space for all fork process ids
 	
-	process_id = (pid_t*)malloc(argc*sizeof(pid_t));
-	fd = (int**)malloc(argc*sizeof(int*)); 
+	// fd = (int**)malloc(argc*sizeof(int*)); 
+	
 	for ( int i = 1; i < argc; i++) {
 		*(process_id+i) = fork(); // fork
-	        *(fd+i) = (int*)malloc(2*sizeof(int));
-		pipe(*(fd+i));	
+	        // *(fd+i) = (int*)malloc(2*sizeof(int)); 
+		// pipe(*(fd+i));	
 	}
 	
-	int children = argc; 
+	// int children = argc; 
 	
-	while (children > 0) {
-		
+	for ( int i = 1; i < argc; ++i) {
+		if(*(process_id+i) == 0) {
+			printf("%i\n", *(process_id+i)); 
+		}
+		else {
+			printf("Parent\n"); 
+		}
 	}	
-
+	
 	
 	// free(process_id);
-	free(lower_range);
-	free(upper_range); 
+	// free(lower_range);
+	// free(upper_range); 
+	free(ranges); 
+       	free(process_id); 	
+	free(parent_write_fd);
+	free(child_write_fd);	
+
 	return 0; 
 }
