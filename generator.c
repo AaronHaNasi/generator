3e96fb57bb440faa7fb5a5e54eddddeeb2c0bf62
Aaron Bager <kingjojo3@gmail.com>
2021-02-22 18:56:54 -0500

    Added seperate function for getting number of primes

--- generator.c
+++ generator.c
@@ -7,21 +7,18 @@
 struct range {
 	int lower;
 	int upper; 
-} *ranges;
+} *ranges; // struct to hold the upper and lower ranges of the numbers 
+
+int getPrimes(int lower, int upper); 
 
 int main(int argc, char** argv) {
 	pid_t *process_id; // dynamic array to hold all the process ids 
-	// int* lower_range; 
-	// int* upper_range;
-	// struct range *ranges;  // struct to hold all the ranges 
+	pid_t parent_id = getpid(); 
 	int** parent_write_fd; // for the parent to write to the children 
        	int** child_write_fd; 	// for the children to write to the parent
 	parent_write_fd = (int**)malloc(argc*sizeof(int*)); 
 	child_write_fd = (int**)malloc(argc*sizeof(int*)); 
-	// lower_range = (int*)malloc(argc*sizeof(int)); 
-	// upper_range = (int*)malloc(argc*sizeof(int));
 	ranges = (struct range*)malloc(argc*sizeof(struct range)); // allocate space for each range 
-	//fd = (int**)malloc(argc*sizeof(int*));  
 	for ( int i = 1; i < argc; ++i) {
 		(ranges+i)->lower = atoi(strtok(argv[i], ":")); // place lower range into struct 
 	       	(ranges+i)->upper = atoi(strtok(NULL, ":"));  // place upper range into struct
@@ -52,32 +49,40 @@ int main(int argc, char** argv) {
 	for ( int i = 1; i < argc; i++) {
 		*(process_id+i) = fork(); // fork
 	        // *(fd+i) = (int*)malloc(2*sizeof(int)); 
-		// pipe(*(fd+i));	
+		// pipe(*(fd+i));i	
 	}
 	
 	// int children = argc; 
-	
+	// children go here 
 	for ( int i = 1; i < argc; ++i) {
 		if(*(process_id+i) == 0) { // child 
-			close(*(*(fd+1)+j)); // closing write end of pipe, using different pipe to write to parent  
-		}
-		else { // parent 
-			printf("Parent\n"); 
-			for ( int j = 1; j < argc; ++j) {
-				close(*(*(fd)+j)); // close read end of pipe, using different pipe to get information from child
-				write(*(*(fd+1)+j), (ranges+j), sizeof(struct range)); // give each child a struct to work with   
-			}
+			sleep(1);
+		       	printf("child\n"); 	
+			struct range *current_range; 
+			close(*(*(parent_write_fd+1)+i)); // closing write end of pipe, using different pipe to write to parent 
+				
+			read(*(*(parent_write_fd)+i), current_range, sizeof(struct range));  
+			return 0; 
+		       		
 		}
 	}	
 	
-	
-	// free(process_id);
-	// free(lower_range);
-	// free(upper_range); 
+	// Parent should start here
+	printf("parent\n"); 
+	for (int i = 1; i < argc; ++i) {
+		close(parent_write_fd[i][0]); // close read end of pipe, using different pipe to get information from child
+		write(parent_write_fd[i][1], &ranges[i], sizeof(struct range)); 
+	}
 	free(ranges); 
        	free(process_id); 	
 	free(parent_write_fd);
 	free(child_write_fd);	
 
 	return 0; 
+}
+
+int getPrimes(int lower, int upper) {
+	int numberOfPrimes;
+
+	return numberOfPrimes;
 }
